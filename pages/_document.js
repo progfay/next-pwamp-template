3e3a70b7e2ff41d002d5083fee60dde1ecdddd02
progfay <shun1856shun@gmail.com>
2018-06-20 18:12:23 +0900

    feat(AMP): add support for AMP

--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,31 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import Manifest from 'next-manifest/manifest'
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
-  render () {
+  render() {
     return (
-      <html>
+      <html amp=''>
         <Head>
           <title>next-pwa-template</title>
-          <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
           <Manifest href='/static/manifest/manifest.json' themeColor='#FFF' initialScale='1' />
+
+          <meta charSet='utf-8' />
+          <link rel='canonical' href='/' />
+          <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
+
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' />
+          <style amp-custom=''>{`
+            html, body {
+              width: 100vw;
+              height: 100vh;
+              overflow: hidden;
+            }
+          `}</style>
+          <script async src='https://cdn.ampproject.org/v0.js' />
         </Head>
         <body className='custom_class'>
           <Main />
